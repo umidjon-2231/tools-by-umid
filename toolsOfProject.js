3e634fb1f5bc504090fe3142e02082d87ae0da9b
umidjon-2231 <83820489+umidjon-2231@users.noreply.github.com>
2021-09-05 18:38:06 +0500

    nothing

--- toolsOfProject.js
+++ toolsOfProject.js
@@ -1,14 +1,17 @@
 import {useEffect, useState} from "react"
+import {toast} from "react-toastify"
 
 
 export const useThemeDetector = () => {
 
+    const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
     const [isDarkTheme, setIsDarkTheme] = useState(false);
 
 
     useEffect(() => {
-        const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
-        setIsDarkTheme(getCurrentTheme)
+        let theme=getCurrentTheme()
+        toast.success(theme.toString())
+        setIsDarkTheme(getCurrentTheme())
     }, []);
 
     return {isDarkTheme, nameTheme: isDarkTheme?'dark':'light'};
